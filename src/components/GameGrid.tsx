165895cbbbe1d69b84b862f0462a7963c6efadea
junmalaubang <junmalaubang@gmail.com>
2023-10-15 18:38:50 +0800

    add a dynamic page heading

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -21,7 +21,7 @@ const GameGrid = ({gameQuery}: Props)=> {
     return (
         <>
         {error && <Text>{error}</Text> }
-        <SimpleGrid columns={{sm: 1, md: 2, lg: 3}} padding={3} spacing={3} >
+        <SimpleGrid columns={{sm: 1, md: 2, lg: 3}} spacing={3} >
             
             {isLoading && skeletons.map( (skeleton) => 
             (<GameCardContainer key={skeleton}>
